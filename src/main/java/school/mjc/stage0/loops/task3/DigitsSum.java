8e178772eeae8b6715a9b9960e9de2a11969ca7a
Muslimbek Soliyev <abuabdulloh0709@gmail.com>
2023-01-11 23:29:54 +0500

    module4 task3 loops

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,14 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
-
+    public void printDigitsSum(int t) {
+        int sumOfDigits = 0;
+        t = Math.abs(t);
+        int lengthOfT = String.valueOf(t).length();
+        for (int i = 0; i < lengthOfT; i++) {
+            sumOfDigits += t % 10;
+            t /= 10;
+        }
+        System.out.println(sumOfDigits);
     }
 }
